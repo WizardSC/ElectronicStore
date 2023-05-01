b6ba7e1af6d9b41514c22a6f6c5fb3c17847d165
Huỳnh Phúc Toàn <97122589+WizardSC@users.noreply.github.com>
2023-05-01 21:47:50 +0700

    update stored procedure

---
+++ database/Stored Procedure/sp_ThemCTPN (thêm chi tiết phiếu nhập).sql
@@ -0,0 +1,38 @@
+﻿select * from CTPhieuNhap
+--Thêm CTPN khi đứng tại 1 chi nhánh bất kỳ
+alter proc sp_ThemCTPN
+	@MaPN nvarchar(20),
+	@MaSP nvarchar(20),
+	@TenSP nvarchar(50),
+	@SoLuong int,
+	@DonGia int,
+	@ThanhTien int
+as
+begin
+	if not exists(select MaPN from CTPHIEUNHAP where MaPN = @MaPN)
+		begin
+			print N'Không tồn tại phiếu nhập ở chi nhánh này'
+			return
+		end
+	if exists(select * from LINK_TO_CN1.ElectronicStore.dbo.CTPhieuNhap where MaPN = @MaPN and MaSP = @MaSP)
+		begin
+			print N'Đã tồn tại chi tiết phiếu nhập'
+		end
+	else if exists(select * from LINK_TO_CN2.ElectronicStore.dbo.CTPhieuNhap where MaPN = @MaPN and MaSP = @MaSP)
+		begin
+			print N'Đã tồn tại chi tiết phiếu nhập'
+		end
+	else if exists(select * from LINK_TO_CN3.ElectronicStore.dbo.CTPhieuNhap where MaPN = @MaPN and MaSP = @MaSP)
+		begin
+			print N'Đã tồn tại chi tiết phiếu nhập'
+		end
+	else
+		begin
+			insert into CTPHIEUNHAP (MaPN, MaSP, TenSP, SoLuong, DonGia,ThanhTien) values (@MaPN, @MaSP, @TenSP, @SoLuong, @DonGia, @ThanhTien)
+		end
+			
+end
+
+exec sp_ThemCTPN 'PN003', 'SP001', 'A', 20, 10000, 200000
+
+select * from CTPHIEUNHAP
---
+++ database/Stored Procedure/sp_ThemPN (thêm phiếu nhập mới).sql
@@ -0,0 +1,40 @@
+﻿--Thêm phiếu nhập mới khi đứng ở một chi nhánh bất kỳ
+alter proc sp_ThemPN
+	@MaPN nvarchar(20),
+	@NgayLap date,
+	@TongTien int,
+	@MaNV nvarchar(20),
+	@MaNCC nvarchar(20)
+as
+begin
+	if exists (select MaPN from LINK_TO_CN1.ElectronicStore.dbo.PhieuNhap where MaPN = @MaPN)
+		begin
+			print N'Đã tồn tại mã phiếu nhập trong hệ thống'
+		end
+	else if exists (select MaPN from LINK_TO_CN2.ElectronicStore.dbo.PhieuNhap where MaPN = @MaPN )
+		begin
+			print N'Đã tồn tại mã phiếu nhập trong hệ thống'
+		end
+	else if exists (select MaPN from LINK_TO_CN3.ElectronicStore.dbo.PhieuNhap where MaPN = @MaPN )
+		begin
+			print N'Đã tồn tại mã phiếu nhập trong hệ thống'
+		end
+	else
+		if exists(select MaNV from nhanvien where MaNV = @MaNV)
+			begin
+				insert into PhieuNhap (MaPN, NgapLap, TongTien, MaNV, MaNCC) values (@MaPN, @NgayLap, @TongTien, @MaNV, @MaNCC) 
+			end
+		else
+			print N'Nhân viên không thuộc chi nhánh này'
+end
+
+
+exec sp_ThemPN 'PN065', '2023-05-01', 10000, 'NV003', 'NCC001'
+
+select * from LINK_TO_CN1.ElectronicStore.dbo.PhieuNhap
+union
+select * from LINK_TO_CN2.ElectronicStore.dbo.PhieuNhap
+union
+select * from LINK_TO_CN3.ElectronicStore.dbo.PhieuNhap
+
+select * from nhanvien
