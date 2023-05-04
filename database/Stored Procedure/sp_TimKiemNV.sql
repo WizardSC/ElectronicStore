4d478b3baa3fc156418d4634ae7579b91ab106a7
Huỳnh Phúc Toàn <97122589+WizardSC@users.noreply.github.com>
2023-05-04 21:02:22 +0700

    update stored procedure

---
+++ database/Stored Procedure/sp_TimKiemNV.sql
@@ -0,0 +1,39 @@
+﻿--Tìm kiếm nhân viên (xuất ra MaNV, Họ, Tên, Năm sinh) theo mã NV khi đứng ở 1 chi nhánh bất kỳ
+alter proc sp_TimKiemNV
+	@MaNV nvarchar(20)
+as
+begin
+
+	declare @Ho nvarchar(20)
+	declare @Ten nvarchar(20)
+	declare @NamSinh int
+	if exists (select * from LINK_TO_CN1.ElectronicStore.dbo.NhanVien where MaNV = @MaNV)
+		begin
+			select @Ho = Ho, @Ten = Ten, @NamSinh = NamSinh 
+			from LINK_TO_CN1.ElectronicStore.dbo.NhanVien 
+			where MaNV = @MaNV
+			print @MaNV + ' ' + @Ho + ' ' + @Ten + ' ' + CAST(@NamSinh as nvarchar(20))
+		end
+	else if exists (select * from LINK_TO_CN2.ElectronicStore.dbo.NhanVien where MaNV = @MaNV)
+		begin
+			select @Ho = Ho, @Ten = Ten, @NamSinh = NamSinh 
+			from LINK_TO_CN2.ElectronicStore.dbo.NhanVien 
+			where MaNV = @MaNV
+			print @MaNV + ' ' + @Ho + ' ' + @Ten + ' ' + CAST(@NamSinh as nvarchar(20))
+		end
+	else if exists (select * from LINK_TO_CN3.ElectronicStore.dbo.NhanVien where MaNV = @MaNV)
+		begin
+			select @Ho = Ho, @Ten = Ten, @NamSinh = NamSinh 
+			from LINK_TO_CN3.ElectronicStore.dbo.NhanVien 
+			where MaNV = @MaNV
+			print @MaNV + ' ' + @Ho + ' ' + @Ten + ' ' + CAST(@NamSinh as nvarchar(20))
+		end
+	else
+		print N'Nhân viên không tồn tại'
+end
+
+exec sp_TimKiemNV 'NV003'
+
+select * from nhanvien
+
+select (year(getdate()) - NamSinh) from nhanvien where manv = 'NV003'
---
+++ database/Stored Procedure/sp_TimKiemNVTheoTuoi.sql
@@ -0,0 +1,46 @@
+﻿--Tìm kiếm những nhân viên tại 1 chi nhánh bất kỳ có tuổi nằm trong khoảng nhất định 
+alter proc sp_TimKiemNVTheoTuoi
+	@TuoiBD int,
+	@TuoiKT int
+as
+begin
+	declare @rowcount int = 
+	(select sum(tong) as tong1 
+		from (
+			select count(*) as tong from LINK_TO_CN1.ElectronicStore.dbo.NhanVien
+			union
+			select count(*) as tong from LINK_TO_CN2.ElectronicStore.dbo.NhanVien
+			union
+			select count(*) as tong from LINK_TO_CN3.ElectronicStore.dbo.NhanVien
+		) as tbl
+	) 
+	declare @MaNV nvarchar(20)
+	declare @Ho nvarchar(20)
+	declare @Ten nvarchar(20)
+	declare @Tuoi int
+
+	while(@rowcount > 0)
+	begin
+		set @rowcount = @rowcount - 1
+		select @MaNV = MaNV, @Ho = Ho, @Ten = Ten, @Tuoi = NamSinh from 
+			(select * from  LINK_TO_CN1.ElectronicStore.dbo.NhanVien
+			 union
+			 select * from LINK_TO_CN2.ElectronicStore.dbo.NhanVien
+			 union
+			 select * from LINK_TO_CN3.ElectronicStore.dbo.NhanVien) as temp
+		order by MaNV desc
+		offset @rowcount rows
+		fetch next 1 rows only
+		if (@TuoiBD <= (year(getdate()) - @Tuoi) and (year(getdate()) - @Tuoi) <= @TuoiKT)
+			print @MaNV + ' ' + @Ho + ' ' +@Ten + ' ' + cast(@Tuoi as nvarchar(20))
+	end
+end
+exec sp_TimKiemNVTheoTuoi 20, 30
+
+go
+
+select MaNV, Year(getdate()) - NamSinh from  LINK_TO_CN1.ElectronicStore.dbo.NhanVien
+union
+select MaNV, Year(getdate()) - NamSinh from  LINK_TO_CN2.ElectronicStore.dbo.NhanVien
+union
+select MaNV, Year(getdate()) - NamSinh from  LINK_TO_CN3.ElectronicStore.dbo.NhanVien
